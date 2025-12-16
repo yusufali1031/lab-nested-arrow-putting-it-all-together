3d07de29eb0c9bc5c9cc84f1d8a75ac2b22514bc
yusuf <yusuf.ali1031@gmail.com>
2025-12-16 14:33:35 +0200

    Complete lab

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+function createLoginTracker({ username, password }) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === password) {
+      return "Login successful";
+    }
+
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
