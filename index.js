ab0de7d6f80c786e545379b1322cb419dd439593
yusuf <yusuf.ali1031@gmail.com>
2026-03-29 22:44:28 +0200

    Complete nested arrow login tracker lab

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
+
+  return (passwordAttempt) => {
+    if (isLocked) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      attemptCount = 0;
+      return "Login successful";
+    }
+
+    attemptCount++;
+
+    if (attemptCount >= 3) {
+      isLocked = true;
+      return "Account locked due to too many failed login attempts";
+    }
+
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 
 
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  createLoginTracker
 };
